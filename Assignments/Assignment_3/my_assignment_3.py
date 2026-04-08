6c0a05a867e6bbedb39e96a954f86c2e62413043
lakshyajain06 <jlakshya06@gmail.com>
2026-04-08 17:06:47 -0400

    finished assignmment 3

--- Assignments/Assignment_3/my_assignment_3.py
+++ Assignments/Assignment_3/my_assignment_3.py
@@ -42,3 +42,54 @@ def forward_kinematics(theta1, theta2, theta3):
     T_0_ee = T_0_1 @ T_1_2 @ T_2_3 @ T_3_ee
     return T_0_ee[:3, 3]
 
+
+def inverse_kinematics_with_optimizer(target):
+
+    def get_error(theta, desired_position):
+        scalar_difference = np.linalg.norm(forward_kinematics(*theta) - desired_position)
+        return scalar_difference
+    
+    initial_guess = [0, 0, 0]
+    res = scipy.optimize.minimize(get_error, initial_guess, args=(target,) )
+    return res.x
+
+def inverse_kinematics_with_gradient(target):
+    def get_cost(theta, target):
+        curr_pos = forward_kinematics(*theta)
+        sum_sqr_err = np.square(target - curr_pos).sum()
+        mean_abs_err = np.absolute(target - curr_pos).mean()
+
+        return sum_sqr_err, mean_abs_err
+    
+    def get_gradient(theta, target):
+        def compute_partial(index, theta, target):
+            dx = 0.0001
+            diff = np.array([0.0 , 0.0 , 0.0])
+            diff[index] = 0.0001
+            
+            upper = get_cost(theta + diff, target)[0]
+            lower = get_cost(theta - diff, target)[0]
+            return (upper - lower) / (2 * dx)
+        
+        grad = []
+
+        for i in range(len(theta)):
+            grad.append(compute_partial(i, theta, target))
+
+        return np.array(grad)
+    
+    max_steps = 5000
+    tolerance = 0.0002
+    lbd = 1
+
+    curr_guess = np.array([0.0, 0.0, 0.0])
+    for i in range(max_steps):
+        curr_cost = get_cost(curr_guess, target)
+        if (curr_cost[1]) < tolerance:
+            return curr_guess
+        
+        grad = get_gradient(curr_guess, target)
+        curr_guess -= lbd * grad
+
+    return curr_guess
+
